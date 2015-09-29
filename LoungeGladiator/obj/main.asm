dd379eeee055db8be7bfa064e270952a0d61cc49
Alberto <alberto.martinez.martinez.1@gmail.com>
2015-09-29 15:47:52 +0200

    commit main.c

--- LoungeGladiator/obj/main.asm
+++ LoungeGladiator/obj/main.asm
@@ -1,7 +1,7 @@
 ;--------------------------------------------------------
 ; File Created by SDCC : free open source ANSI-C Compiler
-; Version 3.5.0 #9253 (Sep 22 2015) (CYGWIN)
-; This file was generated Tue Sep 29 15:41:30 2015
+; Version 3.5.0 #9253 (Sep 26 2015) (CYGWIN)
+; This file was generated Tue Sep 29 15:46:34 2015
 ;--------------------------------------------------------
 	.module main
 	.optsdcc -mz80
@@ -10,8 +10,23 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
-	.globl _cpct_drawStringM1
+	.globl _loadMap
+	.globl _game
+	.globl _menu
+	.globl _init
+	.globl _cpct_getScreenPtr
+	.globl _cpct_setPalette
+	.globl _cpct_fw2hw
+	.globl _cpct_waitVSYNC
+	.globl _cpct_setVideoMode
+	.globl _cpct_drawStringM0
+	.globl _cpct_drawSolidBox
+	.globl _cpct_drawSpriteMasked
+	.globl _cpct_isKeyPressed
+	.globl _cpct_scanKeyboard_f
 	.globl _cpct_memset
+	.globl _cpct_disableFirmware
+	.globl _g_palette
 ;--------------------------------------------------------
 ; special function registers
 ;--------------------------------------------------------
@@ -43,12 +58,49 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;src/main.c:22: void main(void) {
+;src/main.c:34: void init(){
 ;	---------------------------------
-; Function main
+; Function init
 ; ---------------------------------
-_main::
-;src/main.c:27: cpct_memset(video_memory_start, 0, 0x4000);
+_init::
+;src/main.c:35: cpct_disableFirmware();
+	call	_cpct_disableFirmware
+;src/main.c:36: cpct_setVideoMode(0);
+	xor	a, a
+	push	af
+	inc	sp
+	call	_cpct_setVideoMode
+	inc	sp
+;src/main.c:37: cpct_fw2hw(g_palette,4);
+	ld	de,#_g_palette
+	ld	a,#0x04
+	push	af
+	inc	sp
+	push	de
+	call	_cpct_fw2hw
+	pop	af
+	inc	sp
+;src/main.c:38: cpct_setPalette(g_palette,4);
+	ld	de,#_g_palette
+	ld	a,#0x04
+	push	af
+	inc	sp
+	push	de
+	call	_cpct_setPalette
+	pop	af
+	inc	sp
+	ret
+_g_palette:
+	.db #0x00	; 0
+	.db #0x1A	; 26
+	.db #0x06	; 6
+	.db #0x12	; 18
+;src/main.c:42: void menu(){
+;	---------------------------------
+; Function menu
+; ---------------------------------
+_menu::
+;src/main.c:44: cpct_clearScreen(0);
 	ld	hl,#0x4000
 	push	hl
 	xor	a, a
@@ -57,23 +109,226 @@ _main::
 	ld	h, #0xC0
 	push	hl
 	call	_cpct_memset
-;src/main.c:30: cpct_drawStringM1("Welcome to CPCtelera!",
-	ld	de,#___str_0
-	ld	hl,#0x0001
+;src/main.c:46: memptr = cpct_getScreenPtr(VMEM,20,10);
+	ld	hl,#0x0A14
 	push	hl
-	ld	hl,#0xC3C0
+	ld	hl,#0xC000
+	push	hl
+	call	_cpct_getScreenPtr
+;src/main.c:47: cpct_drawStringM0("Super Menu",memptr,2,3);
+	ex	de,hl
+	ld	bc,#___str_0+0
+	ld	hl,#0x0302
 	push	hl
 	push	de
-	call	_cpct_drawStringM1
+	push	bc
+	call	_cpct_drawStringM0
 	ld	hl,#6
 	add	hl,sp
 	ld	sp,hl
-;src/main.c:34: while (1);
-00102$:
-	jr	00102$
+;src/main.c:49: memptr = cpct_getScreenPtr(VMEM,18,180);
+	ld	hl,#0xB412
+	push	hl
+	ld	hl,#0xC000
+	push	hl
+	call	_cpct_getScreenPtr
+;src/main.c:50: cpct_drawStringM0("Pulsa Intro",memptr,4,5);
+	ex	de,hl
+	ld	bc,#___str_1+0
+	ld	hl,#0x0504
+	push	hl
+	push	de
+	push	bc
+	call	_cpct_drawStringM0
+	ld	hl,#6
+	add	hl,sp
+	ld	sp,hl
+;src/main.c:52: do{
+00101$:
+;src/main.c:53: cpct_scanKeyboard_f();
+	call	_cpct_scanKeyboard_f
+;src/main.c:54: }while(!cpct_isKeyPressed(Key_Enter));
+	ld	hl,#0x4000
+	call	_cpct_isKeyPressed
+	ld	a,l
+	or	a, a
+	jr	Z,00101$
+	ret
 ___str_0:
-	.ascii "Welcome to CPCtelera!"
+	.ascii "Super Menu"
 	.db 0x00
+___str_1:
+	.ascii "Pulsa Intro"
+	.db 0x00
+;src/main.c:60: void game(){
+;	---------------------------------
+; Function game
+; ---------------------------------
+_game::
+	push	ix
+	ld	ix,#0
+	add	ix,sp
+	ld	hl,#-8
+	add	hl,sp
+	ld	sp,hl
+;src/main.c:61: TPlayer p = { 0,100 };
+	ld	hl,#0x0000
+	add	hl,sp
+	ld	(hl),#0x00
+	ld	hl,#0x0000
+	add	hl,sp
+	ld	-4 (ix),l
+	ld	-3 (ix),h
+	ld	a,-4 (ix)
+	add	a, #0x01
+	ld	-2 (ix),a
+	ld	a,-3 (ix)
+	adc	a, #0x00
+	ld	-1 (ix),a
+	ld	l,-2 (ix)
+	ld	h,-1 (ix)
+	ld	(hl),#0x64
+;src/main.c:63: u8* sprite = gladis_quieto_dcha;
+	ld	-6 (ix),#<(_gladis_quieto_dcha)
+	ld	-5 (ix),#>(_gladis_quieto_dcha)
+;src/main.c:64: cpct_clearScreen(0);
+	ld	hl,#0x4000
+	push	hl
+	xor	a, a
+	push	af
+	inc	sp
+	ld	h, #0xC0
+	push	hl
+	call	_cpct_memset
+;src/main.c:66: while (1){
+00112$:
+;src/main.c:69: cpct_waitVSYNC();
+	call	_cpct_waitVSYNC
+;src/main.c:72: memptr = cpct_getScreenPtr(VMEM,p.x,p.y);
+	ld	l,-2 (ix)
+	ld	h,-1 (ix)
+	ld	b,(hl)
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	c, (hl)
+	push	bc
+	ld	hl,#0xC000
+	push	hl
+	call	_cpct_getScreenPtr
+;src/main.c:73: cpct_drawSolidBox(memptr,0,4,16);
+	ex	de,hl
+	ld	hl,#0x1004
+	push	hl
+	xor	a, a
+	push	af
+	inc	sp
+	push	de
+	call	_cpct_drawSolidBox
+	pop	af
+	pop	af
+	inc	sp
+;src/main.c:77: cpct_scanKeyboard_f();
+	call	_cpct_scanKeyboard_f
+;src/main.c:78: if(cpct_isKeyPressed(Key_CursorRight) && p.x < 69 ){
+	ld	hl,#0x0200
+	call	_cpct_isKeyPressed
+	ld	a,l
+	or	a, a
+	jr	Z,00108$
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	d,(hl)
+	ld	a,d
+	sub	a, #0x45
+	jr	NC,00108$
+;src/main.c:79: p.x += 1;
+	inc	d
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	(hl),d
+;src/main.c:80: sprite = gladis_quieto_dcha;
+	ld	-6 (ix),#<(_gladis_quieto_dcha)
+	ld	-5 (ix),#>(_gladis_quieto_dcha)
+	jr	00109$
+00108$:
+;src/main.c:81: }else if(cpct_isKeyPressed(Key_CursorLeft) && p.x > 0 ){
+	ld	hl,#0x0101
+	call	_cpct_isKeyPressed
+	ld	a,l
+	or	a, a
+	jr	Z,00104$
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	a,(hl)
+	or	a, a
+	jr	Z,00104$
+;src/main.c:82: p.x -= 1;
+	add	a,#0xFF
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	(hl),a
+;src/main.c:83: sprite = gladis_quieto_izda;
+	ld	-6 (ix),#<(_gladis_quieto_izda)
+	ld	-5 (ix),#>(_gladis_quieto_izda)
+	jr	00109$
+00104$:
+;src/main.c:84: }else  if(cpct_isKeyPressed(Key_Esc)){
+	ld	hl,#0x0408
+	call	_cpct_isKeyPressed
+	ld	a,l
+	or	a, a
+;src/main.c:85: return;
+	jr	NZ,00114$
+00109$:
+;src/main.c:88: memptr = cpct_getScreenPtr(VMEM,p.x,p.y);
+	ld	l,-2 (ix)
+	ld	h,-1 (ix)
+	ld	d,(hl)
+	ld	l,-4 (ix)
+	ld	h,-3 (ix)
+	ld	b,(hl)
+	push	de
+	inc	sp
+	push	bc
+	inc	sp
+	ld	hl,#0xC000
+	push	hl
+	call	_cpct_getScreenPtr
+;src/main.c:89: cpct_drawSpriteMasked(sprite,memptr,4,16);
+	ex	de,hl
+	ld	c,-6 (ix)
+	ld	b,-5 (ix)
+	ld	hl,#0x1004
+	push	hl
+	push	de
+	push	bc
+	call	_cpct_drawSpriteMasked
+	jp	00112$
+00114$:
+	ld	sp, ix
+	pop	ix
+	ret
+;src/main.c:97: void loadMap(){
+;	---------------------------------
+; Function loadMap
+; ---------------------------------
+_loadMap::
+;src/main.c:99: }
+	ret
+;src/main.c:104: void main(void) {
+;	---------------------------------
+; Function main
+; ---------------------------------
+_main::
+;src/main.c:106: init();
+	call	_init
+;src/main.c:109: while(1){
+00102$:
+;src/main.c:110: menu();
+	call	_menu
+;src/main.c:111: game();
+	call	_game
+	jr	00102$
 	.area _CODE
 	.area _INITIALIZER
 	.area _CABS (ABS)
