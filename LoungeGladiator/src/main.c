043171aa6a8553a28752e696dd3122ac2a7c70ca
Alberto <alberto.martinez.martinez.1@gmail.com>
2015-09-29 15:42:25 +0200

    prueba

--- LoungeGladiator/src/main.c
+++ LoungeGladiator/src/main.c
@@ -17,18 +17,99 @@
 //------------------------------------------------------------------------------
 
 #include <cpctelera.h>
+#include <stdio.h>
+#include "single.h"
+
+
+#define VMEM (u8*)0xC000
+
+typedef struct 
+{
+  u8 x,y; //posicion personaje
+
+}TPlayer;
+
+const u8 g_palette[4] = { 0,26,6,18 };
+/*INICIALIZACION*/
+void init(){
+   cpct_disableFirmware();
+   cpct_setVideoMode(0);
+   cpct_fw2hw(g_palette,4);
+   cpct_setPalette(g_palette,4);
+}
+
+/*MENU*/
+void menu(){
+   u8* memptr;
+   cpct_clearScreen(0);
+
+   memptr = cpct_getScreenPtr(VMEM,20,10);
+   cpct_drawStringM0("Super Menu",memptr,2,3);
+
+    memptr = cpct_getScreenPtr(VMEM,18,180);
+   cpct_drawStringM0("Pulsa Intro",memptr,4,5);
+   
+   do{
+    cpct_scanKeyboard_f();
+   }while(!cpct_isKeyPressed(Key_Enter));
+
+}
+
+/*JUEGO*/
+
+void game(){
+   TPlayer p = { 0,100 };
+   u8* memptr;
+   u8* sprite = g_tile_single_0;
+   cpct_clearScreen(0);
+
+   while (1){
+     
+      //Esperar el retrazado
+      cpct_waitVSYNC();
+
+      //Desdibujar personaje   
+      memptr = cpct_getScreenPtr(VMEM,p.x,p.y);
+      cpct_drawSolidBox(memptr,0,11,49);
+
+
+      //Comprobar teclado
+      cpct_scanKeyboard_f();
+      if(cpct_isKeyPressed(Key_CursorRight) && p.x < 69 ){
+         p.x += 1;
+         sprite = g_tile_single_0;
+      }else if(cpct_isKeyPressed(Key_CursorLeft) && p.x > 0 ){
+         p.x -= 1;
+         sprite = g_tile_single_1;
+      }else  if(cpct_isKeyPressed(Key_Esc)){
+         return;
+      }
+      //Dibujar personaje   
+      memptr = cpct_getScreenPtr(VMEM,p.x,p.y);
+      cpct_drawSpriteMasked(sprite,memptr,11,49);
+   }
+}
+
+
+
+/*MAPA*/
+
+void loadMap(){
+  
+}
+
+
+/*EMPIEZA EL CODIGO*/
 
 void main(void) {
-   u8* video_memory_start  = (u8*)0xC000;
-   u8  character_line_size = 0x050;
 
-   // Clear Screen
-   cpct_memset(video_memory_start, 0, 0x4000);
-
-   // Draw String on the middle of the screen
-   cpct_drawStringM1("Welcome to CPCtelera!",
-                     video_memory_start + character_line_size * 12,
-                     1, 0);
+   init();
+    FILE *fr;
+   //loadMap();
    // Loop forever
-   while (1);
+   while(1){
+      menu();
+      game();
+   }
+      
 }
